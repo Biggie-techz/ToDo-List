eacf5e9f2764dc20691581bb0356237646256c1d
Adewole Ayonitemi <adewoleayonitemi@gmail.com>
2024-08-21 22:00:35 +0100

    Update

--- script.js
+++ script.js
@@ -4,37 +4,30 @@ let list = document.getElementById('list');
 let tasks = [];
 let taskId = 0;
 
-add.addEventListener('click', () => {
-    let taskText = input.value.trim();
-    if (taskText === '') {
-        alert('Please enter a task');
-    } else {
-        tasks.push({ id: taskId++, task: taskText, completed: false });
-        updateList();
-        input.value = '';
-    }
-});
+
 function updateList() {
     list.innerHTML = '';
     tasks.forEach((task) => {
         let li = document.createElement('li');
         li.id = task.id;
         li.innerHTML = `
-                <input type="checkbox" ${task.completed ? 'checked' : ''}>
-                <span>${task.task}</span>
-                <div class="actions">
-                <button id="delete">Delete</button>
-                <button id="edit">Edit</button>
-                </div>
-            `;
+        <input type="checkbox" ${task.completed ? 'checked' : ''}>
+        <span>${task.task}</span>
+        <div class="actions">
+        <button id="delete">Delete</button>
+        <button id="edit">Edit</button>
+        </div>
+        `;
         list.appendChild(li);
 
-
+        // CHECKBOX
         let checkbox = li.querySelector('input[type="checkbox"]');
         checkbox.addEventListener('change', () => {
             task.completed = checkbox.checked;
         });
 
+
+        // DELETE BUTTON
         let deleteButton = li.querySelector('#delete');
         deleteButton.addEventListener('click', () => {
             let confirmDelete = confirm('Are you sure you want to delete this task?');
@@ -43,6 +36,8 @@ function updateList() {
                 updateList();
             }
         });
+
+        // EDIT BUTTON
         let editButton = li.querySelector('#edit');
         editButton.addEventListener('click', () => {
             let newTask = prompt('Enter new task', task.task);
@@ -52,4 +47,33 @@ function updateList() {
             }
         });
     });
+
+    // DELETE ALL BUTTON
+    let delAll = document.createElement('button');
+    delAll.innerHTML = 'Delete All';
+    delAll.classList.add('btn');
+    delAll.addEventListener('click', () => {
+        let confirmDelete = confirm('Are you sure you want to delete all tasks?');
+        if (confirmDelete) {
+            tasks = [];
+            updateList();
+            list.innerHTML = '';
+        }
+    });
+    list.appendChild(delAll);
 }
+
+add.addEventListener('click', () => {
+    let taskText = input.value.trim();
+    if (taskText === '') {
+        alert('Please enter a task');
+    } else {
+        tasks.push({ id: taskId++, task: taskText, completed: false });
+        updateList();
+        input.value = '';
+    }
+
+
+});
+
+
